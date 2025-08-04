88d10db7343ec830fc362d83dc5d23f435f23b9c
thecircuitkid <chillithatguy@gmail.com>
2025-08-04 01:26:12 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -9,7 +9,7 @@ antle = turtle.Turtle()
 global coords
 coords = []
 global squaresize
-squaresize = 25
+squaresize = 50
 
 # Functio to move a turtle of your choice  to a specific coordi with spped(0) and heading(0) and penup
 def goooo(whichtle, x, y):
@@ -26,7 +26,7 @@ def goround(whichtle, x, y):
     whichtle.pendown()
         
 # Function to draw the grid with 50x50 squares  
-def draw_grid():
+def draw_grid(squaresize):
     goooo(drawtle, -250, -250)
     drawtle.speed(0)
     drawtle.left(90)
@@ -88,7 +88,7 @@ def coord_blot():
         drawtle.dot(6, "red")
 
 # Function to fill a square with a colour and update the coordinates list
-def square_fill(whichtle, colour, angle):
+def square_fill(whichtle, colour, angle, squaresize):
     whichtle.speed(0)
     whichtle.penup()
     whichtle.seth(0)
@@ -119,16 +119,16 @@ def square_fill(whichtle, colour, angle):
     
 # Function to randomise the colours of the grid squares
 # Can be used to randomise GoLs
-def randomise_grid():
-    for i in range(len(coords)):
+def randomise_grid(squaresize):
+    for i in range(len(coords), squaresize):
         goooo(drawtle, coords[i][0]+squaresize/2, coords[i][1]+squaresize/2)
         colour = random.choice(["white", "black"])
-        square_fill(drawtle, colour, drawtle.heading())
+        square_fill(drawtle, colour, drawtle.heading(), squaresize)
         coords[i] = (coords[i][0], coords[i][1], colour)
 
 
 # Function to check the colour under the turtle (WHY is this not built in)
-def colour_check():
+def colour_check(squaresize):
     for i in range(len(coords)):
         if int(coords[i][0]) == round(antle.ycor()-squaresize/2, 0) and int(coords[i][1]) == round(antle.xcor()-squaresize/2, 0):
             return coords[i][2]
@@ -137,7 +137,7 @@ def colour_check():
 
 # Function to move the turtle by a certain amount of 50x50 squares
 # +dy/-dy = up/down, +dx/-dx = left/right
-def coord_move(dy, dx):
+def coord_move(dy, dx, squaresize):
     antle.penup()
     antle.seth(0)
     antle.forward(int(squaresize)*dy)
@@ -146,64 +146,64 @@ def coord_move(dy, dx):
     antle.seth(0)
 
 # Function to draw a smiley face
-def smiley():
-    square_fill(antle, "black", antle.heading())
+def smiley(squaresize):
+    square_fill(antle, "black", antle.heading(), squaresize)
     coord_move(-3, 0)
-    square_fill(antle, "black", antle.heading())
+    square_fill(antle, "black", antle.heading(), squaresize)
     coord_move(0, -2)
-    square_fill(antle, "black", antle.heading())
+    square_fill(antle, "black", antle.heading(), squaresize)
     coord_move(1, -1)
-    square_fill(antle, "black", antle.heading())
+    square_fill(antle, "black", antle.heading(), squaresize)
     coord_move(1, 0)
-    square_fill(antle, "black", antle.heading())
+    square_fill(antle, "black", antle.heading(), squaresize)
     coord_move(1, 1)
-    square_fill(antle, "black", antle.heading())
+    square_fill(antle, "black", antle.heading(), squaresize)
 
 # Function to draw a heart shape
-def heart():
-    square_fill(antle, "red", antle.heading())
+def heart(squaresize):
+    square_fill(antle, "red", antle.heading(), squaresize)
     coord_move(0, -1)
-    square_fill(antle, "red", antle.heading())
+    square_fill(antle, "red", antle.heading(), squaresize)
     coord_move(1, 0)
-    square_fill(antle, "red", antle.heading())
+    square_fill(antle, "red", antle.heading(), squaresize)
     coord_move(0, -1)
-    square_fill(antle, "red", antle.heading())
+    square_fill(antle, "red", antle.heading(), squaresize)
     coord_move(1, 1)
-    square_fill(antle, "red", antle.heading())
+    square_fill(antle, "red", antle.heading(), squaresize)
     coord_move(0, 1)
-    square_fill(antle, "red", antle.heading())
+    square_fill(antle, "red", antle.heading(), squaresize)
 
-def sebbycake():
+def sebbycake(squaresize):
     for i in range(2):
         for i in range(5):
             coord_move(1, 0)
-            square_fill(antle, "burlywood1", antle.heading())
+            square_fill(antle, "burlywood1", antle.heading(), squaresize)
         coord_move(-5, 1)
     for i in range(5):
         coord_move(1, 0)
-        square_fill(antle, "DeepPink1", antle.heading())
+        square_fill(antle, "DeepPink1", antle.heading(), squaresize)
     coord_move(-3, 1)
-    square_fill(antle, "chartreuse1", antle.heading())
+    square_fill(antle, "chartreuse1", antle.heading(), squaresize)
     coord_move(0, 1)
-    square_fill(antle, "orange", antle.heading())
+    square_fill(antle, "orange", antle.heading(), squaresize)
     coord_move(2, -1)
-    square_fill(antle, "chartreuse1", antle.heading())
+    square_fill(antle, "chartreuse1", antle.heading(), squaresize)
     coord_move(0, 1)
-    square_fill(antle, "orange", antle.heading())
+    square_fill(antle, "orange", antle.heading(), squaresize)
 
     
 
 # Function for my original purpose for this project, Langton's Ant
-def the_ant():
+def the_ant(squaresize):
     for i in range(1000):
         antle.speed(1)
         antle.penup()
-        if colour_check() == "white":
-            square_fill(antle, "black", antle.heading())
+        if colour_check(squaresize) == "white":
+            square_fill(antle, "black", antle.heading(), squaresize)
             antle.right(90)
             antle.forward(int(squaresize))
-        elif colour_check() == "black":
-            square_fill(antle, "white", antle.heading())
+        elif colour_check(squaresize) == "black":
+            square_fill(antle, "white", antle.heading(), squaresize)
             antle.left(90)
             antle.forward(int(squaresize))
     
@@ -220,22 +220,22 @@ drawtle.hideturtle()
 goooo(antle, squaresize/2, squaresize/2)
 s.tracer(0)
 # Makes the undeserving Drawtle draw the grid
-draw_grid()
+draw_grid(squaresize)
 
 # Blots the coordinates of the grid
 
 coord_blot()
-randomise_grid()
+#randomise_grid()
 s.tracer(1)
 
 
 # Uncomment the following lines to test the functions
-the_ant()
+the_ant(squaresize)
 
 
 
-#square_fill(antle, "black", antle.heading())
-#square_fill(antle, "white", antle.heading())
+#square_fill(antle, "black", antle.heading(), squaresize)
+#square_fill(antle, "white", antle.heading(), squaresize)
 #smiley()
 #print(coords)
 #coord_move(0, 6)
